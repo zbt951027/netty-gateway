dac904db2d0b8007a0aad803e84a77ce01722d15
zbt <839294848@qq.com>
2020-12-15 17:42:17 +0800

    更新网关部分代码

--- src/main/java/gateway/inbound/HttpInboundServer.java
+++ src/main/java/gateway/inbound/HttpInboundServer.java
@@ -29,27 +29,36 @@ public class HttpInboundServer {
     public void run() throws Exception {
 
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup(16);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.option(ChannelOption.SO_BACKLOG, 128)
+            // 链接请求等待队列的大小
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    // 关闭Nagle算法（该算法会将小数据块合并成一个大的数据块后在发送），true是关闭，如果要求一个比较高的实时性，可以把这个关闭
                     .option(ChannelOption.TCP_NODELAY, true)
+                    // bossGroup KEEPALIVE
                     .option(ChannelOption.SO_KEEPALIVE, true)
+                    // 允许重复使用本地地址和端口
                     .option(ChannelOption.SO_REUSEADDR, true)
+                    // 设置字节发送缓冲区和接收缓冲区的大小
                     .option(ChannelOption.SO_RCVBUF, 32 * 1024)
                     .option(ChannelOption.SO_SNDBUF, 32 * 1024)
+                    // 多个进程或者线程绑定到同一端口,避免惊群问题，并在内核层面实现了负载均衡
                     .option(EpollChannelOption.SO_REUSEPORT, true)
+                    // workerGroup KEEPALIVE
                     .childOption(ChannelOption.SO_KEEPALIVE, true)
-                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
-
-            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO)).childHandler(new HttpInboundInitializer(this.proxyServer));
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpInboundInitializer(this.proxyServer));
 
             Channel ch = b.bind(port).sync().channel();
             logger.info("开启netty http服务器，监听地址和端口为 http://127.0.0.1:" + port + '/');
             ch.closeFuture().sync();
         } finally {
+            // 优雅退出，释放线程组
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
